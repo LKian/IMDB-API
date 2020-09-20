1d0f6ac52e4b85a3dcfe37b7fcebb938df902447
Leah <LeahKahwadjian@gmail.com>
2020-09-20 19:34:31 -0400

    removing unnecessary console log

--- src/server.js
+++ src/server.js
@@ -1,30 +1,32 @@
-require('dotenv').config();
-const apiKey = process.env.API_KEY
+require("dotenv").config();
+const apiKey = process.env.API_KEY;
 console.log("apiKey ", apiKey);
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'))
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use(express.static("public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
-const axios = require('axios');
+const axios = require("axios");
 
 // ROUTES
 
-app.get('/', (req, res) => {
-    res.render('../src/pages/index')
-})
+app.get("/", (req, res) => {
+  res.render("../src/pages/index");
+});
 
-app.get('/movie', async (req, res) => {
-    const { query: { search } } = req;
-    const apiURL = `https://www.omdbapi.com/?t=${search}&apikey=${apiKey}`;
-    console.log("apiURL ", apiURL);
+app.get("/movie", async (req, res) => {
+  const {
+    query: { search }
+  } = req;
+  const apiURL = `https://www.omdbapi.com/?t=${search}&apikey=${apiKey}`;
+  console.log("apiURL ", apiURL);
 
-    const apiData = await axios.get(apiURL);
-    console.log("apiData ", apiData);
-    return apiData;
+  const apiData = await axios.get(apiURL);
+  console.log("apiData ", apiData);
+  return apiData;
 });
 
 const port = 3000;
